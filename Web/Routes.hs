37f954d16b95ff3673307df47af9f89160c1693b
omgbebebe <omgbebebe@gmail.com>
2026-09-03 17:48:46 +0400

    initial

---
+++ Web/Routes.hs
@@ -0,0 +1,10 @@
+module Web.Routes where
+import IHP.RouterPrelude
+import Generated.Types
+import Web.Types
+
+-- The welcome page at '/' is served by the static controller below.
+-- Additional [routes|...|] blocks get appended by `new-controller`.
+[routes|StaticController
+GET /    WelcomeAction
+|]
